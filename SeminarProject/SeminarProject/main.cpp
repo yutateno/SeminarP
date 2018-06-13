a1451e07ae99995e0e8a2163ed8bcdd510972628
Yuuki_Nakagawa <kingwasheart@yahoo.co.jp>
2018-06-13 14:25:10 +0900

    プロジェクトの設定直し

---
+++ SeminarProject/SeminarProject/main.cpp
@@ -0,0 +1,26 @@
+#include "DxLib.h"
+#include "fbxsdk.h"
+
+// プログラムは WinMain から始まります
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	ChangeWindowMode(TRUE);
+
+	FbxManager* manager = FbxManager::Create();
+	
+
+	if (DxLib_Init() == -1)		// ＤＸライブラリ初期化処理
+	{
+		return -1;			// エラーが起きたら直ちに終了
+	}
+
+	DrawPixel(320, 240, GetColor(255, 255, 255));	// 点を打つ
+
+	WaitKey();				// キー入力待ち
+
+	manager->Destroy();	//これを呼んでおけばFBX SDKのメモリ解放忘れはないはずです。
+
+	DxLib_End();				// ＤＸライブラリ使用の終了処理
+
+	return 0;				// ソフトの終了 
+}
