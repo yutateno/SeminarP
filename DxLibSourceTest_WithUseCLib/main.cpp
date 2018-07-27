1339e77cea97c538b05966340c5d07cfc899290a
Yuuki_Nakagawa <kingwasheart@yahoo.co.jp>
2018-07-27 16:01:27 +0900

    剣をプロジェクトに追加 読み込みの確認として剣を表示

--- DxLibSourceTest_WithUseCLib/main.cpp
+++ DxLibSourceTest_WithUseCLib/main.cpp
@@ -29,10 +29,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	MYINPUTPAD::Input::Input();
 	MYINPUTPAD::Input::Update();
 	KeyData::UpDate();
-	int model = MV1LoadModel("media\\姫_変身体\\モーション\\attack.fbx");
+	int model = MV1LoadModel("media\\剣\\sword.fbx");
+
+	VECTOR area = VGet(320.0f, 50.0f, 330.0f);
 
 	// 画面に映る位置に３Ｄモデルを移動
-	MV1SetPosition(model, VGet(320.0f, 50.0f, 30.0f));
+	MV1SetPosition(model, area);
 
 	// ３Ｄモデルの０番目のアニメーションをアタッチする
 	int AttachIndex = MV1AttachAnim(model, 0, -1, FALSE);
@@ -43,22 +45,46 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	// 再生時間の初期化
 	float PlayTime = 0.0f;
 
+	float turn = DX_PI_F / 90;
+	float circru = 0.0f;
+
 	while (ScreenFlip() == 0 && ProcessMessage() == 0 && ClearDrawScreen() == 0 && KeyData::CheckEnd() != 0)
 	{
 		KeyData::UpDate();
 		MYINPUTPAD::Input::Update();
 
-		// 再生時間を進める
-		PlayTime += 0.5f;
+		//// 再生時間を進める
+		//PlayTime += 0.5f;
 
-		// 再生時間がアニメーションの総再生時間に達したら再生時間を０に戻す
-		if (PlayTime >= TotalTime)
+		//// 再生時間がアニメーションの総再生時間に達したら再生時間を０に戻す
+		//if (PlayTime >= TotalTime)
+		//{
+		//	PlayTime = 0.0f;
+		//}
+
+		//// 再生時間をセットする
+		//MV1SetAttachAnimTime(model, AttachIndex, PlayTime);
+
+		if (KeyData::Get(KEY_INPUT_W) >= 1)
 		{
-			PlayTime = 0.0f;
+			area.z += 10.0f;
 		}
+		if (KeyData::Get(KEY_INPUT_A) >= 1)
+		{
+			area.x += -10.0f;
+		}
+		if (KeyData::Get(KEY_INPUT_S) >= 1)
+		{
+			area.z += -10.0f;
+		}
+		if (KeyData::Get(KEY_INPUT_D) >= 1)
+		{
+			area.x += 10.0f;
+		}
+		circru += turn;
+		MV1SetRotationXYZ(model, VGet(circru, circru, circru));
 
-		// 再生時間をセットする
-		MV1SetAttachAnimTime(model, AttachIndex, PlayTime);
+		MV1SetPosition(model, area);
 
 		// ３Ｄモデルの描画
 		MV1DrawModel(model);
