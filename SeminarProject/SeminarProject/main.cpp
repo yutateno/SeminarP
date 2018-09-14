02137c2ec7c3eeb13293d48bf927d092330775f6
NakagawaYuki <kingwasheart@yahoo.co.jp>
2018-09-14 23:52:00 +0900

    街灯のモデルを少し修正。FPSを表示するよう調整。

--- SeminarProject/SeminarProject/main.cpp
+++ SeminarProject/SeminarProject/main.cpp
@@ -4,6 +4,25 @@
 int winWidth = 1920;
 int winHeight = 1080;
 
+int counter = 0, FpsTime[2] = { 0, }, FpsTime_i = 0;
+double Fps = 0.0;
+char Key[256];
+
+void FpsTimeFanction() {
+	if (FpsTime_i == 0)
+		FpsTime[0] = GetNowCount();               //1周目の時間取得
+	if (FpsTime_i == 49) {
+		FpsTime[1] = GetNowCount();               //50周目の時間取得
+		Fps = 1000.0f / ((FpsTime[1] - FpsTime[0]) / 50.0f);//測定した値からfpsを計算
+		FpsTime_i = 0;//カウントを初期化
+	}
+	else
+		FpsTime_i++;//現在何周目かカウント
+	if (Fps != 0)
+		DrawFormatString(1700, 0, GetColor(0, 0, 0), "FPS %.1f", Fps); //fpsを表示
+	return;
+}
+
 void ProjectInit()
 {
 #ifdef _DEBUG
@@ -146,6 +165,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 			manager->Update(controllNumber);
 		}
+
+		FpsTimeFanction();
 	}
 
 	// 削除
